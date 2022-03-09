75b5676672fbcb4a0300e31ffa91c3b8f61f44a3
saifoelloh <saifoelloh@gmail.com>
2022-03-09 22:12:24 +0700

    fix: minor update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config();
+
+console.log(process.env.DATABASE_URL);
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, APP_NAME, DATABASE_URL } = process.env;
