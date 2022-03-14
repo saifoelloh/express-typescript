3aac82c85e2d03484e9c2c88a82a57c6b4cc8cc9
saifoelloh <saifoelloh@gmail.com>
2022-03-14 22:39:01 +0700

    fix: naming convention

--- src/routes/chapters.route.ts
+++ src/routes/chapters.route.ts
@@ -1,38 +1,35 @@
 import { Router } from 'express';
-import { CreateCourseDto } from '@dtos/courses.dto';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
 import authMw from '@/middlewares/auth.middleware';
-import { imageUploadMw } from '@/middlewares/media.middleware';
-import { ImageDto } from '@/dtos/image.dto';
-import ChapterController from '@/controllers/chapters.controller';
 import { CreateChapterDto } from '@/dtos/chapters.dto';
+import ChoiceController from '@/controllers/choice.controller';
 
 class ChaptersRoute implements Routes {
-  public path = '/chapters';
+  public path = '/choices';
   public router = Router();
-  public chapterController = new ChapterController();
+  public chapterController = new ChoiceController();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.chapterController.getChapters);
-    this.router.get(`${this.path}/:id`, this.chapterController.getChapterById);
+    this.router.get(`${this.path}`, this.chapterController.getChoices);
+    this.router.get(`${this.path}/:id`, this.chapterController.getChoiceById);
     this.router.post(
       `${this.path}`,
       authMw(),
       validationMiddleware(CreateChapterDto, 'body'),
-      this.chapterController.createChapter,
+      this.chapterController.createChoice,
     );
     this.router.patch(
       `${this.path}/:id`,
       authMw(),
       validationMiddleware(CreateChapterDto, 'body', true),
-      this.chapterController.updateChapter,
+      this.chapterController.updateChoice,
     );
-    this.router.delete(`${this.path}/:id`, authMw(), this.chapterController.deleteChapter);
+    this.router.delete(`${this.path}/:id`, authMw(), this.chapterController.deleteChoice);
   }
 }
 
--- src/services/chapters.service.ts
+++ src/services/chapters.service.ts
@@ -6,7 +6,7 @@ import { CreateChapterDto } from '@/dtos/chapters.dto';
 
 class ChapterService {
   readonly prisma = new PrismaClient();
-  readonly courseChapter = this.prisma.chapter;
+  readonly chapter = this.prisma.chapter;
 
   public async findAllChapter(
     pagination: Pagination<Chapter>,
@@ -14,14 +14,14 @@ class ChapterService {
   ): Promise<[Chapter[], number]> {
     const { show = 10, page = 0, orderBy = [{ createdAt: 'desc' }] } = pagination;
     const payload: [Chapter[], number] = await this.prisma.$transaction([
-      this.courseChapter.findMany({
+      this.chapter.findMany({
         skip: show * page,
         take: show,
         orderBy,
         where: filter,
         include: { course: true, _count: true },
       }),
-      this.courseChapter.count({ where: filter }),
+      this.chapter.count({ where: filter }),
     ]);
     return payload;
   }
@@ -30,37 +30,37 @@ class ChapterService {
     if (_.isEmpty(option)) throw new HttpException(400, 'Bad Request');
 
     const { key, value } = option;
-    const course = await this.courseChapter.findFirst({
+    const course = await this.chapter.findFirst({
       where: { [key]: value },
       include: { course: true, quizzes: true },
     });
     return course;
   }
 
-  public async createChapter(courseChapterData: CreateChapterDto): Promise<Chapter> {
-    if (_.isEmpty(courseChapterData)) throw new HttpException(400, "You're not course");
+  public async createChapter(chapterDaat: CreateChapterDto): Promise<Chapter> {
+    if (_.isEmpty(chapterDaat)) throw new HttpException(400, "You're not course");
 
-    const course = await this.courseChapter.create({
-      data: courseChapterData,
+    const course = await this.chapter.create({
+      data: chapterDaat,
       include: { course: true, quizzes: true },
     });
     return course;
   }
 
-  public async updateChapter(courseChapterId: string, courseChapterData: CreateChapterDto): Promise<Chapter> {
-    if (_.isEmpty(courseChapterData)) throw new HttpException(400, "You're not courseData");
+  public async updateChapter(chapterId: string, chapterData: CreateChapterDto): Promise<Chapter> {
+    if (_.isEmpty(chapterData)) throw new HttpException(400, "You're not courseData");
 
-    const updateCourseData = await this.courseChapter.update({
-      where: { id: courseChapterId },
-      data: courseChapterData,
+    const updateCourseData = await this.chapter.update({
+      where: { id: chapterId },
+      data: chapterData,
     });
     return updateCourseData;
   }
 
-  public async deleteChapter(courseId: string): Promise<Chapter> {
-    if (_.isEmpty(courseId)) throw new HttpException(400, 'Bad Request');
+  public async deleteChapter(chapterId: string): Promise<Chapter> {
+    if (_.isEmpty(chapterId)) throw new HttpException(400, 'Bad Request');
 
-    const deleteCourseData = await this.courseChapter.delete({ where: { id: courseId } });
+    const deleteCourseData = await this.chapter.delete({ where: { id: chapterId } });
     return deleteCourseData;
   }
 }
